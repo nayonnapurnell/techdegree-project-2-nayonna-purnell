100a5d7788d2aac33eb06b55dea6c90ea7c8ea0d
“Nayonna <Nayonna Purnell>
2019-11-17 22:53:27 -0500

    Added the functions with no body

--- js/script.js
+++ js/script.js
@@ -5,18 +5,6 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const numberOfPages = 10;
 const studentDetailsDiv = document.querySelector('.student-details');
 
@@ -38,6 +26,10 @@ const studentDetailsDiv = document.querySelector('.student-details');
        "invoke" the function 
 ***/
 
+const showPage = (list, page) => {
+
+}
+
 
 
 
@@ -46,8 +38,9 @@ const studentDetailsDiv = document.querySelector('.student-details');
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = (list) => {
+
+}
 
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+ 
