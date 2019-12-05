ab7e9f4964086334640b53adaa1d17bd3568fa9c
“Nayonna <Nayonna Purnell>
2019-12-05 15:00:35 -0500

    created the showPage function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,31 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Developer: Nayonna Purnell
+Date: December 5, 2019
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-const numberOfPages = 10;
-const studentDetailsDiv = document.querySelector('.student-details');
-
+const list = document.querySelectorAll('.student-list'); // The list of students 
+const studentsPerPage = 10;  //The number of students per page
+const 
 
 
+// showPage tells the program to post 10 list items per page.
+const showPage = (list, studentsPerPage) => {
+  
+  const startIndex = (list * studentsPerPage) - studentsPerPage;
+  const endIndex = (list * studentsPerPage) - 1;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-const showPage = (list, page) => {
-
+  for(var i = 0; i < list.length; i++) {
+    if (i >= startIndex && i <= endIndex)
+      {
+        list[i].style.display === 'none' ;
+      }
+    else {
+        list[i].style.display === 'block';
+      }
+  }
 }
-
-
 
 
 /*** 
@@ -38,7 +33,27 @@ const showPage = (list, page) => {
    functionality to the pagination buttons.
 ***/
 
+// /* Variables to store buttons */
+// const page1 = document.querySelector('#1');
+// const page2 = document.querySelector('#2');
+// const page3 = document.querySelector('#3');
+// const page4 = document.querySelector('#4');
+// const page5 = document.querySelector('#5');
+
+// btn3.addEventListener('click', () => {
+
+//   // Invoke the function to set the third element 
+
+//   // Helpful log statement to test function
+//   console.log('Third button is functional!');
+// });
+
 const appendPageLinks = (list) => {
+
+  
+  
+
+
 
 }
 
