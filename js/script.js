b802117a9dd207ce9164e765887355811996a62d
“Nayonna <Nayonna Purnell>
2019-12-10 12:24:16 -0500

    Created the pagination divs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ Date: December 5, 2019
    
 const list = document.querySelectorAll('.student-list'); // The list of students 
 const studentsPerPage = 10;  //The number of students per page
-const 
 
 
 // showPage tells the program to post 10 list items per page.
@@ -33,23 +32,20 @@ const showPage = (list, studentsPerPage) => {
    functionality to the pagination buttons.
 ***/
 
-// /* Variables to store buttons */
-// const page1 = document.querySelector('#1');
-// const page2 = document.querySelector('#2');
-// const page3 = document.querySelector('#3');
-// const page4 = document.querySelector('#4');
-// const page5 = document.querySelector('#5');
-
-// btn3.addEventListener('click', () => {
-
-//   // Invoke the function to set the third element 
-
-//   // Helpful log statement to test function
-//   console.log('Third button is functional!');
-// });
-
 const appendPageLinks = (list) => {
+  
+  const totalNumberOfPages = Math.ceil(list.length/studentsPerPage);
 
+  const div = document.createElement('div');
+  div.className = 'pagination';
+  const page = document.createElement('.page');
+  div.appendChild(page);
+  const newUl = document.createElement('ul');
+  newUl = document.createElement('.pagination');
+
+  
+
+ 
   
   
 
