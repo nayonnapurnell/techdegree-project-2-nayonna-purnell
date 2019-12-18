481d69802933590b8b6e62e5f23e6402a1d0c829
“Nayonna <Nayonna Purnell>
2019-12-17 21:45:20 -0500

    Added the latest code

--- js/script.js
+++ js/script.js
@@ -1,69 +1,71 @@
 /******************************************
 Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+FSJS project 2 - listItems Filter and Pagination
 Developer: Nayonna Purnell
 Date: December 5, 2019
 ******************************************/
    
-const list = document.querySelectorAll('.student-list'); // The list of students 
+const studentlistItems = document.querySelectorAll('.student-listItems'); // The listItems of all students 
+const listItems = document.getElementsByTagName('li'); //grabs each student individually
 const studentsPerPage = 10;  //The number of students per page
 
 
-// showPage tells the program to post 10 list items per page.
-const showPage = (list, studentsPerPage) => {
-  
-  const startIndex = (list * studentsPerPage) - studentsPerPage;
-  const endIndex = (list * studentsPerPage) - 1;
 
-  for(var i = 0; i < list.length; i++) {
+// showPage tells the program to post 10 listItems items per page.
+const showPage = (listItems, studentsPerPage) => {
+  
+  const startIndex = (listItems * studentsPerPage) - studentsPerPage;
+  const endIndex = (listItems * studentsPerPage) - 1;
+
+  for(var i = 0; i < listItems.length; i++) {
     if (i >= startIndex && i <= endIndex)
       {
-        list[i].style.display === 'none' ;
+        listItems[i].style.display === 'none' ;
       }
     else {
-        list[i].style.display === 'block';
+        listItems[i].style.display === 'block';
       }
   }
 }
-
-
+(showPage(listItem, 1));
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-const appendPageLinks = (list) => {
-  
-  const totalNumberOfPages = Math.ceil(list.length/studentsPerPage);
+const appendPageLinks = (listItems) => {
+  //number of pages needed for the total amount of student list items
+  //In this example there is 54
+  const totalNumberOfPages = Math.ceil(listItems.length/studentsPerPage);
 
-  const div = document.createElement('div');
-  div.className = 'pagination';
-  const page = document.createElement('.page');
-  div.appendChild(page);
-  const newUl = document.createElement('ul');
-  newUl = document.createElement('.pagination');
 
-  
+//Select the page and append the pagination
+  let page = document.querySelectorAll('.page');
+  let pagination = document.createElement('div');
+  pagination.className='pagination';
+  page.appendChild(pagination);
+
+  //Create the Ul and append it to the pagination
+  let paginationUl = document.createElement('ul');
+  pagination.appendChild(paginationUl);
 
   for (i = 0; i < totalNumberOfPages.length; i++) {
-    const li = document.createElement('li');
-    page.appendChild(li);
-    const a = document.createElement('a');
-    li.appendChild(a);
-    a.textContent = '#';
+     let li = document.createElement('li');
+     let a = document.createElement('a');
+     paginationUl.appendChild(li);
+     a.href = '#';
 
-    a.addEventListener('click', (event))
-  }
-
-
-
- 
-  
-  
-
-
-
+    //An event listener for the 'A Tags'
+     a.addEventListener('click', (event) => {
+     let pageLinks = document.getElementsByTagName('a');
+     for (i = 0; i < pageLinks.length; i++) {
+      pageLinks[i].className = ' ';
+     }
+     event.target.className = 'active';
+     
+      showPage(li, event.target.textContent);
+    });
 }
+}
+appendPageLinks(listItems);
 
-
- 
